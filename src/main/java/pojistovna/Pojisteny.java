9fef6fb071d449d4cddaaf864ab00ed603d92d02
Wikidbeaver <zaryk-1@seznam.cz>
2025-05-23 15:06:41 +0200

    Initial commit of console version

---
+++ src/main/java/pojistovna/Pojisteny.java
@@ -0,0 +1,31 @@
+package pojistovna;
+
+// Trida reprezentujici pojisteneho
+public class Pojisteny {
+    private String jmeno;
+    private String prijmeni;
+    private int vek;
+    private String telefon;
+
+    // Konstruktor udaju o pojistenem
+    public Pojisteny(String jmeno, String prijmeni, int vek, String telefon) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.vek = vek;
+        this.telefon = telefon;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    // Metoda pro vypis informace o pojistenem
+    @Override
+    public String toString() {
+        return jmeno + " " + prijmeni + ", věk: " + vek + ", telefon: " + telefon;
+    }
+}

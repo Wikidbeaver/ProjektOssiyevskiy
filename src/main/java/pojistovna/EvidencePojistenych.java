f7cf98551f46bcb52e6c7a28ecb2f3e5995a9238
Wikidbeaver <zaryk-1@seznam.cz>
2025-05-23 15:06:21 +0200

    Initial commit of console version

--- src/main/java/pojistovna/EvidencePojistenych.java
+++ src/main/java/pojistovna/EvidencePojistenych.java
@@ -1,3 +1,5 @@
+package pojistovna;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
--- src/main/java/pojistovna/Main.java
+++ src/main/java/pojistovna/Main.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+package pojistovna;
+
 import java.util.Scanner;
 
 // Hlavni trida
